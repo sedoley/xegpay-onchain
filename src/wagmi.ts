280b90161af2feb74057d01868e3c101e3d1d94d
sedoley <71331799+sedoley@users.noreply.github.com>
2026-05-11 01:40:19 -0500

    Update wagmi.ts

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,51 +1,105 @@
 'use client';
+
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+
 import {
+
   coinbaseWallet,
+
   metaMaskWallet,
+
   rainbowWallet,
+
 } from '@rainbow-me/rainbowkit/wallets';
+
 import { useMemo } from 'react';
+
 import { http, createConfig } from 'wagmi';
+
 import { base } from 'wagmi/chains';
+
 import { NEXT_PUBLIC_WC_PROJECT_ID } from './config';
 
+
+
 export function useWagmiConfig() {
+
   const projectId = NEXT_PUBLIC_WC_PROJECT_ID ?? '';
+
   if (!projectId) {
+
     const providerErrMessage =
+
       'To connect to all Wallets you need to provide a NEXT_PUBLIC_WC_PROJECT_ID env variable';
+
     throw new Error(providerErrMessage);
+
   }
 
+
+
   return useMemo(() => {
+
     const connectors = connectorsForWallets(
+
       [
+
         {
+
           groupName: 'Recommended Wallet',
+
           wallets: [coinbaseWallet],
+
         },
+
         {
+
           groupName: 'Other Wallets',
+
           wallets: [rainbowWallet, metaMaskWallet],
+
         },
+
       ],
+
       {
-        appName: 'XEGpay',
+
+        appName: 'onchainkit',
+
         projectId,
+
       },
+
     );
 
+
+
     const wagmiConfig = createConfig({
-      chains: [base], // Only Base Mainnet
+
+      chains: [base],
+
+      // turn off injected provider discovery
+
       multiInjectedProviderDiscovery: false,
+
       connectors,
+
       ssr: true,
+
       transports: {
-        [base.id]: http(), // Only Mainnet transport
+
+        [base.id]: http(),
+
+        [base.id]: http(),
+
       },
+
     });
 
+
+
     return wagmiConfig;
+
   }, [projectId]);
+
 }
